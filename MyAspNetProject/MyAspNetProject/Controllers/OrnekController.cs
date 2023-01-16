ee06fb26686ce039158ce965ba6e344776955465
berkcangumusisik <gumusisikberkcan@gmail.com>
2023-01-16 16:11:06 +0300

    Controller'a giriş yapıldı

---
+++ MyAspNetProject/MyAspNetProject/Controllers/OrnekController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetProject.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+/*
+ * Controller'da Action Metotları bulunur. 
+ *
+ */
